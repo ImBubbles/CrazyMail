753617c4fc3f7bb50ac3dbfa0648068e626c1b0f
IsaacGarven <isaacg12321@gmail.com>
2025-11-02 02:15:03 -0600

    Added Gemini

--- src/main/java/scan/aiScan.java
+++ src/main/java/scan/aiScan.java
@@ -1,11 +1,21 @@
 package scan;
 
+import com.google.genai.Client;
+import com.google.genai.types.GenerateContentResponse;
 //Receives the basic scan results and uses them to tell AI what to check the email for.
 public class aiScan
 {
 
     public static category main(String sender, String message, category narrowed)
     {
+        Client client = new Client();
+
+        GenerateContentResponse response =
+        client.models.generateContent(
+            "gemini-2.5-flash",
+            "Give me the history of Papa John's",
+            null);
+
         return category.UNFILTERED;
     }
 }
