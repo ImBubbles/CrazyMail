3f3e39504884e757320e22bdeb06d4bb9deb8db2
IsaacGarven <isaacg12321@gmail.com>
2025-11-01 22:50:53 -0500

    More work on scripts

--- scripts/basicScan.java
+++ scripts/basicScan.java
@@ -3,8 +3,13 @@ package scripts;
 //This is used to narrow down the amount of information the LLM needs to check for.
 public class basicScan
 {
-    //School Email Checker, if the email is a school email, it will return true.
-    public static void main(String email, String emailAddress)
+    public enum decizion
+    {
+        SPAM, SCHOOL
+    }
+    
+    //School Email Checker, determines if the email is a school email.
+    public static void main(int uid, String recipient, String sender, String headers, String message)
     {
         
     }
