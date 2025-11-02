771ac60372de739886af87ee1aefc15a3842bc2c
Daniel Villarreal <dalexv2007@outlook.com>
2025-11-02 00:08:04 -0500

    fiwb

---
+++ node_modules/backend/src/users/users.service.ts
@@ -0,0 +1,35 @@
+import { Injectable, Logger } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+
+@Injectable()
+export class UsersService {
+  private readonly logger = new Logger(UsersService.name);
+
+  create(createUserDto: CreateUserDto) {
+    this.logger.log('[POST] /users Request received for email: ${createUserDto.email}');
+    this.logger.debug('Received Full Payload: ', createUserDto);
+
+    return{
+      message: 'User registered successfully.',
+      status: 'success_mock',
+      receivedEmail: createUserDto.email,
+    };
+  }
+
+  findAll() { 
+    return `This action returns all users`;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} user`;
+  }
+
+  update(id: number, updateUserDto: UpdateUserDto) {
+    return `This action updates a #${id} user`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} user`;
+  }
+}
